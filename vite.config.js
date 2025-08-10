e7b043310ab2828e34c0cfbaf822c6853865dad4
Sachin <SachinMourya303@gmail.com>
2025-08-10 13:42:35 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Grocery-app",
-})
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react';
+            if (id.includes('lodash')) return 'vendor-lodash';
+            if (id.includes('chart.js')) return 'vendor-chart';
+            // add more libraries here if needed
+            return 'vendor'; // all other node_modules
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // optional: increase limit
+  }
+});
